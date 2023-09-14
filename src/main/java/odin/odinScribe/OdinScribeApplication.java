7ffb5f3cf40137932010c2115a099285beb116f6
Christopher Munden <cmunden@alumni.unca.edu>
2023-09-14 16:22:32 -0400

    Added Spring Boot project files

---
+++ src/main/java/odin/odinScribe/OdinScribeApplication.java
@@ -0,0 +1,13 @@
+package odin.odinScribe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OdinScribeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OdinScribeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/odin/odinScribe/OdinScribeApplicationTests.java
@@ -0,0 +1,13 @@
+package odin.odinScribe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OdinScribeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
