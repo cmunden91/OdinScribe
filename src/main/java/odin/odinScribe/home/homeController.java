fbf6269028592f151d41e4871d25d2848aaf7bec
Christopher Munden <cmunden@alumni.unca.edu>
2023-09-18 16:34:08 -0400

    Removed unsued import statements

--- src/main/java/odin/odinScribe/home/homeController.java
+++ src/main/java/odin/odinScribe/home/homeController.java
@@ -3,9 +3,7 @@ package odin.odinScribe.home;
 import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class homeController {
