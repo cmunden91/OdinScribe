5da7f42c9a2f4c1a1819a2822234c9dd46464b69
Christopher Munden <cmunden@alumni.unca.edu>
2023-09-18 16:21:09 -0400

    Added proper view and altered controller

--- src/main/java/odin/odinScribe/home/homeController.java
+++ src/main/java/odin/odinScribe/home/homeController.java
@@ -1,12 +1,16 @@
 package odin.odinScribe.home;
 
+import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class homeController {
-	@RequestMapping("/")
-	public String home() {
-		return "Hello World";
+	@GetMapping("/")
+	public String home(@RequestParam(required=false, defaultValue="") String name, Model model) {
+		return "home";
 	}
 }
